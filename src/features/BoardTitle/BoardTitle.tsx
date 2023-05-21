2b5a2ab2afa4eeecf63920c18771e562ebe18d69
Yobuligo <hoffmannpeter82@gmail.com>
2023-05-21 15:06:03 +0200

    Add BaordTitle

--- src/features/BoardTitle/BoardTitle.tsx
+++ src/features/BoardTitle/BoardTitle.tsx
@@ -4,10 +4,36 @@ import { IBoardTitleProps } from "./IBoardTitleProps";
 
 const BoardTitle: React.FC<IBoardTitleProps> = (props) => {
   const [title, setTitle] = useState(props.title);
+  const [enabled, setEnabled] = useState(false);
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
+
+  const toggle = () => {
+    setEnabled((previous) => !previous);
+  };
+
+  const onEnter = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      toggle();
+    }
+  };
   return (
     <div className={styles.boardTitle}>
-      {title}
-      <button>
+      <input
+        
+        className={`${
+          enabled ? styles.boardTitleEnabled : styles.boardTitleDisabled
+        }`}
+        value={title}
+        disabled={!enabled}
+        onKeyUp={onEnter}
+        onChange={onChange}
+        onBlur={toggle}
+      />
+
+      <button onClick={toggle}>
         <span className="material-symbols-outlined">edit</span>
       </button>
     </div>
