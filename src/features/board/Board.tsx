17a9b4bc6a5f8622e51e6132bc8e2ec2e37358fa
Yobuligo <hoffmannpeter82@gmail.com>
2023-05-21 13:50:55 +0200

    Display BoardSections in Board

--- src/features/board/Board.tsx
+++ src/features/board/Board.tsx
@@ -1,12 +1,12 @@
-import Section from "../section/Section";
+import BoardSection from "../boardSection/BoardSection";
 import styles from "./Board.module.css";
 
 const Board: React.FC = () => {
   return (
     <div className={styles.board}>
-      <Section name={"Was lief gut"} />
-      <Section name={"Was lief schlecht"} />
-      <Section name={"Verbesserungsvorschläge"} />
+      <BoardSection name={"Was lief gut"} />
+      <BoardSection name={"Was lief schlecht"} />
+      <BoardSection name={"Verbesserungsvorschläge"} />
     </div>
   );
 };
