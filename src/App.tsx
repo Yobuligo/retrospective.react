b75f1e4c41f4e000be4fc4463dcf630d9df148f0
Yobuligo <hoffmannpeter82@gmail.com>
2023-05-22 15:30:25 +0200

    Add summary page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
 import Homepage from "./pages/Homepage";
+import Summary from "./pages/Summary";
 
 function App() {
+  const [page, setPage] = useState("homepage");
+
+  const content = () => {
+    switch (page) {
+      case "homepage": {
+        return <Homepage />;
+      }
+      case "summary": {
+        return <Summary />;
+      }
+    }
+  };
+
   return (
     <>
-      <Homepage />
+      <button onClick={() => setPage("homepage")}>Board</button>
+      <button onClick={() => setPage("summary")}>Summary</button>
+      {content()}
     </>
   );
 }
---
+++ src/pages/Summary.tsx
@@ -0,0 +1,5 @@
+const Summary: React.FC = () => {
+  return <>Test</>;
+};
+
+export default Summary;
