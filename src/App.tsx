ef5e85795dbbc2712b6cde2e5160b18f4fbaa52e
Yobuligo <hoffmannpeter82@gmail.com>
2023-05-22 15:46:21 +0200

    Install mui for using tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
+import Tab from "@mui/material/Tab";
+import Tabs from "@mui/material/Tabs";
 import { useState } from "react";
 import Homepage from "./pages/Homepage";
 import Summary from "./pages/Summary";
 
 function App() {
-  const [page, setPage] = useState("homepage");
+  const [page, setPage] = useState(0);
 
   const content = () => {
     switch (page) {
-      case "homepage": {
+      case 0: {
         return <Homepage />;
       }
-      case "summary": {
+      case 1: {
         return <Summary />;
       }
     }
@@ -18,8 +20,10 @@ function App() {
 
   return (
     <>
-      <button onClick={() => setPage("homepage")}>Board</button>
-      <button onClick={() => setPage("summary")}>Summary</button>
+      <Tabs value={page} onChange={(_, value) => setPage(value)}>
+        <Tab label="Board" />
+        <Tab label="Summary" />
+      </Tabs>
       {content()}
     </>
   );
