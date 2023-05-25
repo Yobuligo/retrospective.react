637723a12a8a78e67452d2f1db9bd5d14dcf1bc1
Peter <hoffmannpeter82@gmail.com>
2023-05-25 10:03:15 +0200

    Add styling for CommentCardList

--- src/features/comment/commentCardList/CommentCardList.tsx
+++ src/features/comment/commentCardList/CommentCardList.tsx
@@ -1,14 +1,17 @@
 import CommentCard from "../commentCard/CommentCard";
 import { ICommentCardListProps } from "./ICommentCardListProps";
+import styles from './CommentCardList.module.css'
 
 const CommentCardList: React.FC<ICommentCardListProps> = (props) => {
   const items = props.comments.map((comment) => (
-    <CommentCard
-      key={comment.id}
-      comment={comment}
-      commentType={props.commentType}
-      onDelete={props.onDelete}
-    />
+    <div className={styles.commentCardListItem}>
+      <CommentCard
+        key={comment.id}
+        comment={comment}
+        commentType={props.commentType}
+        onDelete={props.onDelete}
+      />
+    </div>
   ));
   return <>{items}</>;
 };
