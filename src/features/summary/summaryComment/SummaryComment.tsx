e06a50713af3f65fcfa63b64e06f3ae0fd9112c9
Yobuligo <hoffmannpeter82@gmail.com>
2023-05-29 11:23:59 +0200

    Add styling for summary comment

--- src/features/summary/summaryComment/SummaryComment.tsx
+++ src/features/summary/summaryComment/SummaryComment.tsx
@@ -1,5 +1,16 @@
 import { ISummaryCommentProps } from "./ISummaryCommentProps";
+import styles from "./SummaryComment.module.css";
 
 export const SummaryComment: React.FC<ISummaryCommentProps> = (props) => {
-  return <>{props.comment.text}</>;
+  return (
+    <div className={styles.summaryComment}>
+      <div className={styles.summaryCommentPositive}>
+        {props.comment.countAgrees}
+      </div>
+      <div className={styles.summaryCommentNegative}>
+        {props.comment.countDisagrees}
+      </div>
+      <div>{props.comment.text}</div>
+    </div>
+  );
 };
