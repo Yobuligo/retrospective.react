c55613bc8841923856dbcd05bdfbd751020f1912
Yobuligo <hoffmannpeter82@gmail.com>
2023-05-31 21:07:33 +0200

    Remove default values

--- src/features/app/AppContextProvider.tsx
+++ src/features/app/AppContextProvider.tsx
@@ -3,7 +3,6 @@ import { useDataAccessObject } from "../../hooks/useDataAccessObject";
 import { useLocalStorage } from "../../hooks/useLocalStorage";
 import { useStateToValue } from "../../hooks/useStateToValue";
 import { IComment } from "../../model/IComment";
-import { VotingState } from "../../model/VotingState";
 import { LanguageType } from "../../types/LanguageType";
 import { IAppContextProviderProps } from "./IAppContextProviderProps";
 
@@ -12,33 +11,9 @@ const AppContextProvider: React.FC<IAppContextProviderProps> = (props) => {
     <AppContext.Provider
       value={{
         comments: {
-          negativeDAO: useDataAccessObject<IComment>([
-            {
-              id: crypto.randomUUID(),
-              countAgrees: 0,
-              countDisagrees: 0,
-              text: "Test Negative",
-              votingState: VotingState.Open,
-            },
-          ]),
-          positiveDAO: useDataAccessObject<IComment>([
-            {
-              id: crypto.randomUUID(),
-              countAgrees: 0,
-              countDisagrees: 0,
-              text: "Test Positive",
-              votingState: VotingState.Open,
-            },
-          ]),
-          proposalDAO: useDataAccessObject<IComment>([
-            {
-              id: crypto.randomUUID(),
-              countAgrees: 0,
-              countDisagrees: 0,
-              text: "Test Proposal",
-              votingState: VotingState.Open,
-            },
-          ]),
+          negativeDAO: useDataAccessObject<IComment>(),
+          positiveDAO: useDataAccessObject<IComment>(),
+          proposalDAO: useDataAccessObject<IComment>(),
         },
         language: useStateToValue<LanguageType>(
           useLocalStorage<LanguageType>("language", LanguageType.en)
