f27352be0793461fd42af2aa6675a47c3ae3526c
Peter <hoffmannpeter82@gmail.com>
2023-06-04 22:22:21 +0200

    Add styling for board add button

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,10 +1,11 @@
 import { BoardAdd } from "../features/board/boardAdd/BoardAdd";
+import styles from "./HomePage.module.css";
 
 const HomePage: React.FC = () => {
   return (
-    <>
+    <div className={styles.homePage}>
       <BoardAdd />
-    </>
+    </div>
   );
 };
 
